faa69f08027de349b74974afb25e6bb0d41900cf
Gerardo Suarez <andresuarezz26@hotmail.com>
2014-09-25 21:11:30 -0500

    Se adicionaron las funcionalidades de los comandos RETR,DELE,RNFR y RNTO

--- src/control/ServerPI.java
+++ src/control/ServerPI.java
@@ -11,6 +11,8 @@ import java.util.Date;
 
 public class ServerPI extends Thread
 {
+	private File apuntador;
+	private boolean isRNFR;
 
 	// Socket y flujo de escritura
 	private Socket sktControl;
@@ -40,6 +42,8 @@ public class ServerPI extends Thread
 			out = new PrintWriter(clientSocket.getOutputStream(), true);
 			// Iniciar el Server DTP
 			this.serverDTP = serverDTP;
+			apuntador = null;
+			isRNFR = false;
 		} catch (IOException e)
 		{
 			System.out.println("Error creando el flujo de transmisión");
@@ -106,6 +110,19 @@ public class ServerPI extends Thread
 						} else if (separacion[0].equalsIgnoreCase("RETR"))
 						{
 							commandRETR(separacion[1]);
+						}
+						//Comando DELE
+						else if(separacion[0].equalsIgnoreCase("DELE"))
+						{
+							commandDELE(separacion[1]);
+						}else if(separacion[0].equalsIgnoreCase("RNFR"))
+						{
+							commandRNFR(separacion[1]);
+							System.out.println(isRNFR);
+							System.out.println(apuntador.toString());
+						} else if(separacion[0].equalsIgnoreCase("RNTO") )
+						{
+							commandRNTO(separacion[1]);
 						}
 
 					}
@@ -236,7 +253,7 @@ public class ServerPI extends Thread
 			out.println(SUCCESS);
 		} catch (Exception e)
 		{
-			out.println(ERROR);
+			out.println("ERROR");
 		}
 	}
 
@@ -248,13 +265,110 @@ public class ServerPI extends Thread
 	public void commandRETR(String nombreArchivo)
 	{
 		try
-		{
-			serverDTP.sendFile(nombreArchivo);
-			System.out.println("Se envían los datos al cliente");
-			out.println(SUCCESS);
+		{	
+			File f = new File(serverDTP.getCurrentPath()+"/"+nombreArchivo);
+			if(f.exists()&&f.isFile()){
+				out.println("successful_retr");
+				serverDTP.sendFile(nombreArchivo);	
+				System.out.println("Se envian los datos al cliente");
+				
+				
+			}else{
+				System.out.println("El archivo no existe");
+				out.println("error_retr");
+			}
+			
+			
+			
 		} catch (Exception e)
 		{
 			out.println(ERROR);
+		}
+	}	
+	/**
+	 * 
+	 * @param nombreArchivo
+	 */
+	public void commandDELE(String nombreArchivo)
+	{
+		try
+		{	
+			File f = new File(serverDTP.getCurrentPath()+"/"+nombreArchivo);
+			if(f.exists()&&f.isFile()){
+				System.out.println("entre al metodo dele");
+				if (f.delete()) {
+					out.println("successful_dele");
+				} else {
+					out.println("error_dele");
+				}
+
+			}else{
+				System.out.println("El archivo no existe");
+				out.println("error_dele");
+			}
+			
+			
+			
+		} catch (Exception e)
+		{
+			out.println(ERROR);
+		}
+		
+	}
+	
+	public void commandRNTO(String nuevoNombre)
+	{
+		try
+		{	System.out.println("entra al rnto");
+			if(apuntador.exists()&&apuntador.isFile()&&(apuntador!=null)&&isRNFR)
+			{
+				System.out.println("el apuntador existe");
+				File nuevoArchivo = new File(serverDTP.getCurrentPath()+"/"+nuevoNombre);
+				if (apuntador.renameTo(nuevoArchivo)) {
+					System.out.println("el archivo SE RENOMBRO");
+					out.println("successful_rename");
+					System.out.println("se cambio el nombre al apuntador");
+					isRNFR = false;
+				} else {
+					System.out.println("FALLA AL RENOMBRAR");
+					out.println("error_rename");
+					isRNFR = false;
+				}
+			}else{
+				isRNFR = false;
+				System.out.println("El archivo no existe");
+				out.println("error_dele");
+			}
+
+		} catch (Exception e)
+		{
+			isRNFR = false;
+			out.println(ERROR);
+		}
+	}
+	
+	public void commandRNFR(String nombreArchivo)
+	{
+		try
+		{	
+			File f = new File(serverDTP.getCurrentPath()+"/"+nombreArchivo);
+			if(f.exists()&&f.isFile())
+			{
+				isRNFR = true;
+				apuntador = f;
+				out.println("successful_rnfr");
+			}else{
+				isRNFR = false;
+				System.out.println("El archivo no existe");
+				out.println("error_rnfr");
+				apuntador = null;
+			}
+
+		} catch (Exception e)
+		{
+			isRNFR = false;
+			out.println(ERROR);
+			apuntador = null;
 		}
 	}
 }
