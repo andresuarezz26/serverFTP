0dbf059d81c9d94a623f4c4cfd0fe3ccd0fd22d7
csacanam <csacanam@gmail.com>
2014-10-19 16:50:26 -0500

    Se agrega el log en el ServerPI

--- src/control/ServerPI.java
+++ src/control/ServerPI.java
@@ -8,6 +8,9 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Date;
+import java.util.logging.Level;
+
+import utilities.MyLogger;
 
 public class ServerPI extends Thread
 {
@@ -36,6 +39,16 @@ public class ServerPI extends Thread
 	 */
 	public ServerPI(Socket clientSocket, ServerDTP serverDTP)
 	{
+
+		try
+		{
+			MyLogger.setup();
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+			throw new RuntimeException("Problems with creating the log files");
+		}
+
 		try
 		{
 			this.sktControl = clientSocket;
@@ -53,7 +66,8 @@ public class ServerPI extends Thread
 
 	public void run()
 	{
-		System.out.println("Got a client");
+		MyLogger.logger.log(Level.INFO, "Accede un cliente");
+
 		while (true)
 		{
 			if (sktControl.isClosed())
@@ -70,6 +84,7 @@ public class ServerPI extends Thread
 					}
 
 					String comando = in.readLine();
+					MyLogger.logger.log(Level.INFO, comando);
 
 					String[] separacion = comando.split(" ");
 					// 1. Comandos sin parámetros
@@ -79,6 +94,8 @@ public class ServerPI extends Thread
 						if (comando.equalsIgnoreCase("END"))
 						{
 							out.close();
+							MyLogger.logger.log(Level.INFO, "Termina el cliente");
+
 							// Comando desconocido
 						} else if (comando.equalsIgnoreCase("LIST"))
 						{
@@ -88,6 +105,8 @@ public class ServerPI extends Thread
 						} else
 						{
 							out.println(ERROR);
+							MyLogger.logger.log(Level.INFO, "Comando desconocido");
+
 						}
 
 					}
@@ -104,23 +123,24 @@ public class ServerPI extends Thread
 						else if (separacion[0].equalsIgnoreCase("LIST"))
 						{
 							commandLIST(separacion[1]);
+
 						} else if (separacion[0].equalsIgnoreCase("STOR"))
 						{
 							commandSTOR(separacion[1]);
+
 						} else if (separacion[0].equalsIgnoreCase("RETR"))
 						{
 							commandRETR(separacion[1]);
 						}
-						//Comando DELE
-						else if(separacion[0].equalsIgnoreCase("DELE"))
+						// Comando DELE
+						else if (separacion[0].equalsIgnoreCase("DELE"))
 						{
 							commandDELE(separacion[1]);
-						}else if(separacion[0].equalsIgnoreCase("RNFR"))
+
+						} else if (separacion[0].equalsIgnoreCase("RNFR"))
 						{
 							commandRNFR(separacion[1]);
-							System.out.println(isRNFR);
-							System.out.println(apuntador.toString());
-						} else if(separacion[0].equalsIgnoreCase("RNTO") )
+						} else if (separacion[0].equalsIgnoreCase("RNTO"))
 						{
 							commandRNTO(separacion[1]);
 						}
@@ -161,10 +181,15 @@ public class ServerPI extends Thread
 			{
 				sb.append(file + ";");
 			}
+
+			MyLogger.logger.log(Level.INFO, sb.toString());
+
 			return sb.toString();
 
 		} else
 		{
+			MyLogger.logger.log(Level.WARNING, "El parámetro no es un directorio");
+
 			return ERROR;
 		}
 
@@ -182,6 +207,7 @@ public class ServerPI extends Thread
 		{
 			serverDTP.setCurrentPath(System.getProperty("user.dir") + "/root");
 			out.println(SUCCESS);
+			MyLogger.logger.log(Level.INFO, "Se cambia de directorio");
 
 		} else
 		{
@@ -193,15 +219,20 @@ public class ServerPI extends Thread
 				{
 					serverDTP.setCurrentPath(serverDTP.getCurrentPath() + "/" + rutaRelativa);
 					out.println(SUCCESS);
+					MyLogger.logger.log(Level.INFO, "Se cambia de directorio");
 
 				} else
 				{
 					out.println(ERROR);
+					MyLogger.logger.log(Level.WARNING, "La carpeta no existe");
+
 				}
 
 			} else
 			{
 				out.println(ERROR);
+				MyLogger.logger.log(Level.WARNING, "El archivo no existe");
+
 			}
 		}
 	}
@@ -223,15 +254,21 @@ public class ServerPI extends Thread
 				Date fechaModificacion = new Date(f.lastModified());
 				String resultado = "Tamaño: " + f.length() + " bytes;" + "Oculto: " + f.isHidden() + ";" + "Última modificación: " + fechaModificacion.toString();
 				out.println(resultado);
+				MyLogger.logger.log(Level.INFO, resultado);
+
 			} else
 			{
 				String respuestaList = getListOfDirectory(serverDTP.getCurrentPath());
 				out.println(respuestaList);
+				MyLogger.logger.log(Level.INFO, respuestaList);
+
 			}
 
 		} else
 		{
 			out.println(ERROR);
+			MyLogger.logger.log(Level.WARNING, "El archivo no existe");
+
 		}
 
 	}
@@ -250,10 +287,13 @@ public class ServerPI extends Thread
 			InputStream is = sktDatos.getInputStream();
 			serverDTP.receiveFile(is, rutaArchivo);
 			System.out.println("Se recibieron los datos del cliente");
+			MyLogger.logger.log(Level.INFO, "Se recibieron los datos del cliente");
 			out.println(SUCCESS);
 		} catch (Exception e)
 		{
 			out.println("ERROR");
+			MyLogger.logger.log(Level.WARNING, "El archivo no se pudo almacenar");
+
 		}
 	}
 
@@ -265,26 +305,30 @@ public class ServerPI extends Thread
 	public void commandRETR(String nombreArchivo)
 	{
 		try
-		{	
-			File f = new File(serverDTP.getCurrentPath()+"/"+nombreArchivo);
-			if(f.exists()&&f.isFile()){
+		{
+			File f = new File(serverDTP.getCurrentPath() + "/" + nombreArchivo);
+			if (f.exists() && f.isFile())
+			{
 				out.println("successful_retr");
-				serverDTP.sendFile(nombreArchivo);	
+				serverDTP.sendFile(nombreArchivo);
 				System.out.println("Se envian los datos al cliente");
-				
-				
-			}else{
+				MyLogger.logger.log(Level.INFO, "Se envían los datos al cliente");
+
+			} else
+			{
+				MyLogger.logger.log(Level.WARNING, "El archivo no existe");
 				System.out.println("El archivo no existe");
 				out.println("error_retr");
+
 			}
-			
-			
-			
+
 		} catch (Exception e)
 		{
+			MyLogger.logger.log(Level.WARNING, "No se pudo enviar el archivo");
 			out.println(ERROR);
 		}
-	}	
+	}
+
 	/**
 	 * 
 	 * @param nombreArchivo
@@ -292,73 +336,88 @@ public class ServerPI extends Thread
 	public void commandDELE(String nombreArchivo)
 	{
 		try
-		{	
-			File f = new File(serverDTP.getCurrentPath()+"/"+nombreArchivo);
-			if(f.exists()&&f.isFile()){
-				System.out.println("entre al metodo dele");
-				if (f.delete()) {
+		{
+			File f = new File(serverDTP.getCurrentPath() + "/" + nombreArchivo);
+			if (f.exists() && f.isFile())
+			{
+				if (f.delete())
+				{
+					MyLogger.logger.log(Level.INFO, "Se elimina el archivo");
+
 					out.println("successful_dele");
-				} else {
+				} else
+				{
+					MyLogger.logger.log(Level.WARNING, "No se puede eliminar el archivo");
+
 					out.println("error_dele");
 				}
 
-			}else{
-				System.out.println("El archivo no existe");
-				out.println("error_dele");
-			}
-			
-			
-			
-		} catch (Exception e)
-		{
-			out.println(ERROR);
-		}
-		
-	}
-	
-	public void commandRNTO(String nuevoNombre)
-	{
-		try
-		{	System.out.println("entra al rnto");
-			if(apuntador.exists()&&apuntador.isFile()&&(apuntador!=null)&&isRNFR)
+			} else
 			{
-				System.out.println("el apuntador existe");
-				File nuevoArchivo = new File(serverDTP.getCurrentPath()+"/"+nuevoNombre);
-				if (apuntador.renameTo(nuevoArchivo)) {
-					System.out.println("el archivo SE RENOMBRO");
-					out.println("successful_rename");
-					System.out.println("se cambio el nombre al apuntador");
-					isRNFR = false;
-				} else {
-					System.out.println("FALLA AL RENOMBRAR");
-					out.println("error_rename");
-					isRNFR = false;
-				}
-			}else{
-				isRNFR = false;
+				MyLogger.logger.log(Level.WARNING, "El archivo no existe");
+
 				System.out.println("El archivo no existe");
 				out.println("error_dele");
 			}
 
 		} catch (Exception e)
 		{
+			MyLogger.logger.log(Level.WARNING, "Hubo problemas en el flujo de la información");
+
+			out.println(ERROR);
+		}
+
+	}
+
+	public void commandRNTO(String nuevoNombre)
+	{
+		try
+		{
+			if (apuntador.exists() && apuntador.isFile() && (apuntador != null) && isRNFR)
+			{
+				File nuevoArchivo = new File(serverDTP.getCurrentPath() + "/" + nuevoNombre);
+				if (apuntador.renameTo(nuevoArchivo))
+				{
+					MyLogger.logger.log(Level.INFO, "Se cambia el nombre del archivo");
+					out.println("successful_rename");
+					isRNFR = false;
+
+				} else
+				{
+					MyLogger.logger.log(Level.WARNING, "No se pudo renombrar");
+					out.println("error_rename");
+					isRNFR = false;
+				}
+			} else
+			{
+				isRNFR = false;
+				MyLogger.logger.log(Level.WARNING, "El archivo no existe");
+				out.println("error_dele");
+			}
+
+		} catch (Exception e)
+		{
+			MyLogger.logger.log(Level.WARNING, "No se pudo cambiar el nombre");
 			isRNFR = false;
 			out.println(ERROR);
 		}
 	}
-	
+
 	public void commandRNFR(String nombreArchivo)
 	{
 		try
-		{	
-			File f = new File(serverDTP.getCurrentPath()+"/"+nombreArchivo);
-			if(f.exists()&&f.isFile())
+		{
+			File f = new File(serverDTP.getCurrentPath() + "/" + nombreArchivo);
+			if (f.exists() && f.isFile())
 			{
 				isRNFR = true;
 				apuntador = f;
+				MyLogger.logger.log(Level.WARNING, "Se selecciona el archivo correctamente");
 				out.println("successful_rnfr");
-			}else{
+			} else
+			{
 				isRNFR = false;
+				MyLogger.logger.log(Level.WARNING, "El archivo no existe");
 				System.out.println("El archivo no existe");
 				out.println("error_rnfr");
 				apuntador = null;
@@ -367,6 +426,8 @@ public class ServerPI extends Thread
 		} catch (Exception e)
 		{
 			isRNFR = false;
+			MyLogger.logger.log(Level.WARNING, "No se pudo seleccionar el archivo");
+
 			out.println(ERROR);
 			apuntador = null;
 		}
---
+++ src/utilities/MyLogger.java
@@ -0,0 +1,49 @@
+package utilities;
+
+import java.io.IOException;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Formatter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class MyLogger
+{
+	static private FileHandler fileTxt;
+	static private SimpleFormatter formatterTxt;
+
+	static private FileHandler fileHTML;
+	static private Formatter formatterHTML;
+	
+	public static Logger logger;
+
+	public static void setup() throws IOException
+	{
+
+		// get the global logger to configure it
+		logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+
+		// suppress the logging output to the console
+		Logger rootLogger = Logger.getLogger("");
+		java.util.logging.Handler[] handlers = rootLogger.getHandlers();
+		if (handlers[0] instanceof ConsoleHandler)
+		{
+			rootLogger.removeHandler(handlers[0]);
+		}
+
+		logger.setLevel(Level.INFO);
+		fileTxt = new FileHandler("Logging.txt");
+		fileHTML = new FileHandler("Logging.html");
+
+		// create a TXT formatter
+		formatterTxt = new SimpleFormatter();
+		fileTxt.setFormatter(formatterTxt);
+		logger.addHandler(fileTxt);
+
+		// create an HTML formatter
+		formatterHTML = new MyHtmlFormatter();
+		fileHTML.setFormatter(formatterHTML);
+		logger.addHandler(fileHTML);
+	}
+}
